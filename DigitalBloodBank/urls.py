736d170d50cee622e4f08d33a760a44cadc70d31
aaxyat <aaxyat@gmail.com>
2021-03-01 18:08:54 +0000

    Bugfix V0.1

--- DigitalBloodBank/urls.py
+++ DigitalBloodBank/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
